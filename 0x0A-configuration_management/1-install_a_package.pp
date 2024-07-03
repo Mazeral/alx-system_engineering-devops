8861fd030ce6fb604552b6701284e4b59479551f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-07-03 22:37:04 +0200

    Second task.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # installs flask 2.1.0
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0',
 }
