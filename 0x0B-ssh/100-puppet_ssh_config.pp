63e884b9f24caaa4f0b57909a6cb68550287febc
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-07-04 01:22:53 +0200

    advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,13 @@
 # Connect to a server without typing a password
 
 file_line { 'ssh_indentify_file':
-  path => '~/.ssh/school',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
   match   => '^#?\s*IdentityFile\s+~/.ssh/school$',
 }
